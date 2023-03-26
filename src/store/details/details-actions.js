8f7e3ddcaa17c23808725e98987d29d19823ae75
Yauheny Vial <mr.jk1629003@gmail.com>
2023-03-26 19:41:41 +0300

    feat: add page country

---
+++ src/store/details/details-actions.js
@@ -0,0 +1,29 @@
+export const SET_LOADING = '@@detail/SET_LOADING';
+export const SET_ERROR = '@@detail/SET_ERROR';
+
+export const SET_COUNTRY = '@@details/SET_COUNTRY';
+
+export const CLEAR_DETAILS = '@@detail/CLEAR_DETAILS';
+
+export const setLoading = () => ({
+    type: SET_LOADING,
+})
+export const setError = (err) => ({
+    type: SET_ERROR,
+    payload: err,
+})
+export const setCountry = (country) => ({
+    type: SET_COUNTRY,
+    payload: country,
+})
+
+export const clearDetails = () =>({
+    type:CLEAR_DETAILS,
+})
+
+export const loadCountryByName = (name) => (dispatch, _, {client, api}) => {
+    dispatch(setLoading());
+    client.get(api.searchByCountry(name))
+        .then(({data})=> dispatch(setCountry(data[0])))
+        .catch((err)=> dispatch(setError(err.message)))
+}
