5831ad60d535eef0e3d822caf8b33975e3666cf6
Yauheny Vial <mr.jk1629003@gmail.com>
2023-03-26 20:00:09 +0300

    feat: add info about neighbors countries

--- src/store/details/details-actions.js
+++ src/store/details/details-actions.js
@@ -1,9 +1,8 @@
 export const SET_LOADING = '@@detail/SET_LOADING';
 export const SET_ERROR = '@@detail/SET_ERROR';
-
 export const SET_COUNTRY = '@@details/SET_COUNTRY';
-
 export const CLEAR_DETAILS = '@@detail/CLEAR_DETAILS';
+export const SET_NEIGHBORS = '@@detail/SET_NEIGHBORS';
 
 export const setLoading = () => ({
     type: SET_LOADING,
@@ -16,14 +15,23 @@ export const setCountry = (country) => ({
     type: SET_COUNTRY,
     payload: country,
 })
-
-export const clearDetails = () =>({
-    type:CLEAR_DETAILS,
+export const clearDetails = () => ({
+    type: CLEAR_DETAILS,
+})
+const setNeighbors = (countries) => ({
+    type: SET_NEIGHBORS,
+    payload: countries
 })
 
 export const loadCountryByName = (name) => (dispatch, _, {client, api}) => {
     dispatch(setLoading());
     client.get(api.searchByCountry(name))
-        .then(({data})=> dispatch(setCountry(data[0])))
-        .catch((err)=> dispatch(setError(err.message)))
+        .then(({data}) => dispatch(setCountry(data[0])))
+        .catch((err) => dispatch(setError(err.message)))
+}
+
+export const loadNeighborsByBorder = (borders) => (dispatch, _, {client, api}) => {
+    client.get(api.filterByCode(borders))
+        .then(({data}) => dispatch(setNeighbors(data.map((c) => c.name))))
+        .catch(console.error)
 }
