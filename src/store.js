ded032bf3abf8fee0ea203b6a4986a190c715b1f
Yauheny Vial <mr.jk1629003@gmail.com>
2023-03-28 00:04:23 +0300

    refactor: change render countries from Redux to RTK

--- src/store.js
+++ src/store.js
@@ -1,15 +1,37 @@
 import axios from 'axios';
 import {configureStore} from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {combineReducers} from "@reduxjs/toolkit";
 
 import * as api from './config';
 import {themeReducer} from "./features/theme/theme-slice";
 import {controlsReducer} from "./features/controls/controls-slice";
+import {countryReducer} from "./features/countries/countries-slice";
+
+const rootReducer = combineReducers({
+    theme: themeReducer,
+    controls: controlsReducer,
+    countries: countryReducer,
+})
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        theme: themeReducer,
-        controls: controlsReducer,
-    },
+    reducer: persistedReducer,
     devTools: true,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         thunk: {
@@ -18,6 +40,10 @@ export const store = configureStore({
                 api,
             },
         },
-        serializableCheck: false,
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
     })
 })
+
+export const persistor = persistStore(store);
