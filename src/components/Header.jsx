82f1dcfe2adc1b6eb4a29afbf117c2e0344f3ea2
Yauheny Vial <mr.jk1629003@gmail.com>
2023-03-27 22:59:14 +0300

    refactor: change filters from Redux to RTK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
 
 import {Container} from './Container';
-import {clearControls} from "../store/controls/controls-actions";
-import {ThemeSwither} from "../features/theme/ThemeSwither";
+import {ThemeSwitcher} from "../features/theme/ThemeSwitcher";
+import {useCleanup} from "../features/controls/use-cleanup";
 
 const HeaderEl = styled.header`
   box-shadow: var(--shadow);
@@ -28,18 +27,14 @@ const Title = styled(Link).attrs({
 `;
 
 export const Header = () => {
-    const dispatch = useDispatch();
-
-    const cleanUp = () => {
-        dispatch(clearControls())
-    }
+    const cleanUp = useCleanup();
 
     return (
         <HeaderEl>
             <Container>
                 <Wrapper>
                     <Title onClick={cleanUp}>Where is the world?</Title>
-                    <ThemeSwither/>
+                    <ThemeSwitcher/>
                 </Wrapper>
             </Container>
         </HeaderEl>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,10 +4,11 @@ import {useEffect} from 'react'
 
 import {List} from '../components/List';
 import {Card} from '../components/Card';
-import {Controls} from '../components/Controls';
+import {Controls} from '../features/controls/Controls';
 import {selectCountriesInfo, selectVisibleCountries} from "../store/countries/countries-selectors";
+import {selectControls} from "../features/controls/controls-slice";
 import {loadCountries} from "../store/countries/countries-actions";
-import {selectControls} from "../store/controls/controls-selectors";
+
 
 export const HomePage = () => {
     const navigate = useNavigate();
